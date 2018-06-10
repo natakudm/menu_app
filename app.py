40fb066ab9ef48ab7a38d21469b468c1cb582639
Natalya <natakudm@gmail.com>
2018-06-10 14:32:50 +0300

    Load data from db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from restaurant import Restaurant
 
-my_restaurant = Restaurant('Test_restaurant', 'DK 6 Fw', 'test restaurant description', '/static/img/c1.jpg',
-                           'traditional', None, None)
+# my_restaurant = Restaurant('Test_restaurant', 'DK 6 Fw', 'test restaurant description', '/static/img/c1.jpg',
+                             # 'traditional', None, None)
+# my_restaurant.save_to_db()
 
-my_restaurant.save_to_db()
+my_restaurant = Restaurant.load_from_db_by_name('Test_restaurant')
+print(my_restaurant.description)
--- restaurant.py
+++ restaurant.py
@@ -15,10 +15,17 @@ class Restaurant:
         return "<Restaurant: {}>".format(self.name)
 
     def save_to_db(self):
-        connection = psycopg2.connect(user='postgres', password='7777777', database='menu_app', host='localhost')
-        with connection.cursor() as cursor:
-            cursor.execute('INSERT INTO restaurant(name, location, description, picture, restaurant_type) '
-                           'VALUES (%s, %s, %s, %s, %s)', (self.name, self.location, self.description, self.picture,
-                                                           self.restaurant_type))
-        connection.commit()
-        connection.close()
+        with psycopg2.connect(user='postgres', password='7777777', database='menu_app', host='localhost') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO restaurant(name, location, description, picture, restaurant_type) '
+                               'VALUES (%s, %s, %s, %s, %s)', (self.name, self.location, self.description, self.picture,
+                                                               self.restaurant_type))
+
+    @classmethod
+    def load_from_db_by_name(cls, name):
+        with psycopg2.connect(user='postgres', password='7777777', database='menu_app', host='localhost') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM restaurant WHERE name=%s', (name,))
+                data = cursor.fetchone()
+                return cls(name=data[1], location=data[2], description=data[3], picture=data[4],
+                           restaurant_type=data[5], price=data[6], _id=data[0])
