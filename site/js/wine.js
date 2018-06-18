ecb66d1ab489bbff26dec81051023bcda285b46a
Natalya <natakudm@gmail.com>
2018-06-19 02:38:42 +0300

    Finish styling pages

--- site/js/wine.js
+++ site/js/wine.js
@@ -22,26 +22,26 @@ class UI {
     paint(http) {
         this.wine = document.createElement('div');
             this.wine.className = 'wine';
+        this.wine_image = document.createElement('img');
+            this.wine_image.className = 'wine_img img-responsive img-rounded centered col-lg-6 col-md-6';
+            this.wine_image.setAttribute('src', http.picture);
+            this.wine.appendChild(this.wine_image);
         this.wine_name = document.createElement('h2');
             this.wine_name.className = 'wine_name';
             this.wine_name.textContent = http.name;
             this.wine.appendChild(this.wine_name);
-        this.wine_image = document.createElement('img');
-            this.wine_image.className = 'wine_img';
-            this.wine_image.setAttribute('src', http.picture);
-            this.wine.appendChild(this.wine_image);
         this.description = document.createElement('div');
-            this.description.className = 'wine_description';
+            this.description.className = 'description col-lg-6 col-md-6';
             this.description.textContent = http.description;
             this.wine.appendChild(this.description);
         if(http.price_for_glass !== 'None') {
             this.price_for_glass = document.createElement('div');
-            this.price_for_glass.className = 'price_for_glass';
+            this.price_for_glass.className = 'price';
             this.price_for_glass.textContent = `Price is only ${http.price_for_glass}$ for glass`;
             this.wine.appendChild(this.price_for_glass);
         }
         this.price_for_bottle = document.createElement('div');
-            this.price_for_bottle.className = 'price_for_bottle';
+            this.price_for_bottle.className = 'price';
             this.price_for_bottle.textContent = `Only ${http.price_for_bottle}$ for bottle`;
             this.wine.appendChild(this.price_for_bottle);
 
