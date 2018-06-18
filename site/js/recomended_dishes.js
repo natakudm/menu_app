e704536229a64f8bce09cdd85631215457365cd0
Natalya <natakudm@gmail.com>
2018-06-19 01:49:37 +0300

    Recomended style is finished

--- site/js/recomended_dishes.js
+++ site/js/recomended_dishes.js
@@ -11,20 +11,20 @@ class UI {
     paint(http) {
         for (let i = 0; i < http.length; i++) {
             this.dish = document.createElement('div');
-                this.dish.className = 'dish';
+                this.dish.className = 'dish col-lg-6 col-md-6';
                 this.dish_name = document.createElement('h2');
-            this.dish_name.className = 'dish_name';
+            this.dish_name.className = 'dish_name centered';
                 this.dish_name.textContent = http[i].name;
                 this.dish.appendChild(this.dish_name);
 
             if(http[i].picture !== null) {
                 this.dish_image = document.createElement('img');
-                this.dish_image.className = 'dish_img';
+                this.dish_image.className = 'dish_img img-responsive img-rounded centered';
                 this.dish_image.setAttribute('src', http[i].picture);
                 this.dish.appendChild(this.dish_image);
             }
             this.description = document.createElement('div');
-                this.description.className = 'dish_description';
+                this.description.className = 'description';
                 this.description.textContent = http[i].description;
                 this.dish.appendChild(this.description);
             this.restaurant_name = document.createElement('div');
@@ -41,7 +41,7 @@ class UI {
                 this.wine.appendChild(this.wine_href);
                 this.wine_name = document.createElement('div');
                 this.wine_name.className = 'wine_name';
-                this.wine_name.textContent = `is good with ${http[i].wine_name}`;
+                this.wine_name.textContent = `Is good with ${http[i].wine_name}`;
                 this.wine_href.appendChild(this.wine_name);
                 this.dish.appendChild(this.wine);
             }
