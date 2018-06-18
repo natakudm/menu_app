4a639d67b040ee7010451f1de6e9909a2ecd6702
Natalya <natakudm@gmail.com>
2018-06-18 14:32:41 +0300

    Added cocktails page

---
+++ site/js/cocktails.js
@@ -0,0 +1,37 @@
+let url_page = `http://127.0.0.1:5000/${id_day}/cocktails`;
+
+class UI {
+    constructor() {
+        this.rest_container = document.getElementById('container');
+        this.wait = document.getElementById('wait');
+        this.wait.textContent = '';
+    }
+
+    paint(http) {
+        for (let i = 0; i < http.length; i++) {
+            this.cocktail = document.createElement('div');
+                this.cocktail.className = 'cocktail';
+            this.cocktail_name = document.createElement('h2');
+                this.cocktail_name.className = 'cocktail_name';
+                this.cocktail_name.textContent = http[i].name;
+                this.cocktail.appendChild(this.cocktail_name);
+            this.cocktail_image = document.createElement('img');
+                this.cocktail_image.className = 'cocktail_img';
+                this.cocktail_image.setAttribute('src', http[i].picture);
+                this.cocktail.appendChild(this.cocktail_image);
+            this.description = document.createElement('div');
+                this.description.className = 'cocktail_description';
+                this.description.textContent = http[i].description;
+                this.cocktail.appendChild(this.description);
+            this.price = document.createElement('div');
+                this.price.className = 'price';
+                this.price.textContent = `Only ${http[i].price}$ for glass`;
+                this.cocktail.appendChild(this.price);
+
+            this.rest_container.appendChild(this.cocktail);
+        }
+    }
+
+}
+
+ui = new UI();
