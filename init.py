c11abdafee649252d131cb818d4a66984a214308
Natalya <natakudm@gmail.com>
2018-06-20 10:50:38 +0300

    Added PostreSQL enviroment variable

--- init.py
+++ init.py
@@ -1,5 +1,8 @@
 from database import Database
+import os
 
-Database.initialize(user='postgres', password='7777777', database='menu_app', host='localhost')
+db_url = os.environ.get('DATABASE_URL', 'localhost')
+
+Database.initialize(user='postgres', password='7777777', database='menu_app', host=db_url)
 
 img_url = 'https://menuap.herokuapp.com/site/img/'
