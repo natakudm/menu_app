09b5a12ab268a6684e29bf6b549bd5e75b917d96
Natalya <natakudm@gmail.com>
2018-06-24 18:32:56 +0300

    import db to run.py

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 from database import Database
 import os
 
-db_url = os.environ.get('DATABASE_URL', 'localhost')
-
-Database.initialize(user='postgres', password='7777777', database='menu_app', host=db_url)
+# db_url = os.environ.get('DATABASE_URL', 'localhost')
+#
+# Database.initialize(user='postgres', password='7777777', database='menu_app', host=db_url)
 
 img_url = 'https://menuap.herokuapp.com/site/img/'
--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 from app import app
 import init
+from database import Database
+import os
+
+db_url = os.environ.get('DATABASE_URL', 'localhost')
+
+Database.initialize(user='postgres', password='7777777', database='menu_app', host=db_url)
