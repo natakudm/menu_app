c8163f454e5fc6ec58adcbd99b07c22681dbfdd0
Natalya <natakudm@gmail.com>
2018-06-25 07:27:50 +0300

    Final local site

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 from database import Database
 
-Database.initialize(user='postgres', password='7777777', database='menu_app', host='localhost')
+Database.initialize(user='postgres', password='n7777777', database='menu_app', host='natakudm-803.postgres.pythonanywhere-services.com')
 
 img_url = 'http://127.0.0.1:5000/site/img/'
